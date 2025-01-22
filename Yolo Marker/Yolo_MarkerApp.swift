36c3e08875ee5c7b9121c17467c58fb002eabb74
Peter Kazazes <peter@peterk.co>
2025-01-22 14:18:11 +0600

    Initial Commit

---
+++ Yolo Marker/Yolo_MarkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yolo_MarkerApp.swift
+//  Yolo Marker
+//
+//  Created by p on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Yolo_MarkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Yolo MarkerTests/Yolo_MarkerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Yolo_MarkerTests.swift
+//  Yolo MarkerTests
+//
+//  Created by p on 1/22/25.
+//
+
+import Testing
+@testable import Yolo_Marker
+
+struct Yolo_MarkerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
